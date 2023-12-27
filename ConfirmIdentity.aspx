bcb706cb767c5a6b003630887e85c053bfc3c6e3
NOOR <75979709+nooriq1@users.noreply.github.com>
2023-12-27 23:14:27 +0300

    Add files via upload

---
+++ ConfirmIdentity.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ConfirmIdentity.aspx.cs" Inherits="myproject.ConfirmIdentity" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Caro&display=swap" rel="stylesheet">
+
+
+</head>
+<body>
+
+	
+<div class="container">
+	<div class="screen">
+		<div class="screen__content">
+			<form class="login" id="form1" runat="server">
+				<div class="login__field">
+					<asp:FileUpload ID="FileUpload1" runat="server" />
+				</div>
+				<div class="login__field">
+					<i class="login__icon fas fa-user"></i>
+					<asp:TextBox ID="TextBox1" runat="server" CssClass="login__input" placeholder="name : "></asp:TextBox>
+				</div>
+				<asp:Button ID="Button1" runat="server" Text="check" CssClass="button login__submit" OnClick="Button1_Click" />
+			</form>
+			<div class="social-login">
+				<h3>
+					<asp:Label ID="Label1" runat="server" Text=""></asp:Label></h3>
+
+			</div>
+		</div>
+		
+	</div>	
+	
+</div>
+
+
+</body>
+</html>
---
+++ ConfirmIdentity.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace myproject
+{
+    public partial class ConfirmIdentity : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            bool find = false;
+            string path1 = @"C:\Users\asus\Desktop\my project\main\myproject\myproject\img\output_text_file.txt.txt";
+            
+
+            if (FileUpload1.HasFile )
+            {
+                string fileName = Path.GetFileName(FileUpload1.FileName);
+                string uploadDirectory = Server.MapPath("~/img"); // Define the upload directory
+
+                FileUpload1.SaveAs(Path.Combine(uploadDirectory, fileName));
+
+                // Updated Tesseract path for Arabic language
+                string tesseractPath = @"C:\Program Files\Tesseract-OCR\tesseract.exe";
+                string inputFile = Path.Combine(uploadDirectory, fileName);
+                string outputFile = Path.Combine(uploadDirectory, "output_text_file.txt");
+                string arguments = "\"" + inputFile + "\" \"" + outputFile + "\" -l ara";
+
+                var processInfo = new System.Diagnostics.ProcessStartInfo(tesseractPath, arguments)
+                {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                var process = new System.Diagnostics.Process { StartInfo = processInfo };
+                process.Start();
+                process.WaitForExit();
+
+                // Read the text from the output file
+                string extractedText = File.ReadAllText(@"C:\Users\asus\Desktop\my project\main\myproject\myproject\img\output_text_file.txt.txt");
+
+                // Print or use the extracted text
+                //Response.Write("Extracted Text: " + extractedText);
+
+                string path = @"C:\Users\asus\Desktop\my project\main\myproject\myproject\img\output_text_file.txt.txt";
+
+                if (File.Exists(path))
+                {
+                    string[] lines= File.ReadAllLines(path);
+                    int count = 0;
+                    
+
+                    foreach (string line in lines)
+                    {
+
+
+                        if (line.Contains(TextBox1.Text))
+                        {
+                            ViewState["name"] = line;    
+                            find = true;
+
+                        }
+
+                        count++;
+                    }
+                    
+
+
+                }
+                else
+                {
+
+                    Response.Write("File not found or incorrect path");
+                }
+
+
+
+
+
+            }
+            else
+            {
+                Response.Write("fill all data");
+            }
+
+
+            if (find)
+            {
+
+                Response.Write(" done");
+
+            }
+            else
+            {
+                Response.Write("not done");
+            }
+
+
+        }
+    }
+}
